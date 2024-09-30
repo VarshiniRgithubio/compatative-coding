77c44d488c4720ef95df1dd4e89dab7adfdd33fa
VarshiniR <143378215+VarshiniRgithubio@users.noreply.github.com>
2024-09-30 23:02:54 +0530

    Add files via upload

---
+++ arithamatic operation.py
@@ -0,0 +1,47 @@
+#addition
+print(10+2)
+#substraction
+print(10-2)
+#multiplication
+print(10*5)
+#division
+print(10/3) #it will give the decimal point
+print(10//3)#it will give the int as answer
+#modules
+print(10%3)#it will give the reminder in division
+#power or exponent
+print(10**3)
+#argument assignment += ,-=,*=,==,operator
+x=10
+x=x+10
+print(x)
+#in other way
+x=10
+x+=3
+print(x)
+#-=
+x=10
+x-=3
+print(x)
+#*=
+x=10
+x*=3
+print(x)
+x=100
+x**=3
+print(x)
+#examples based on bodmass
+# rule first divi
+# second  multi or division
+# third addition or  sub
+x=(10+5)-3*5**2
+print(x)
+x=(2+3)*10-3
+print(x)
+#for rounding of
+x=3.7
+print(round(x))
+#we are using the abs function the -value is written as positive
+x=-2.346
+print(abs(x))
+
