9f0300debef64a9db9efac7729154716f3d44e4b
VarshiniR <143378215+VarshiniRgithubio@users.noreply.github.com>
2024-10-04 23:20:07 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,29 @@
+#variables integer
+price=10
+price=50
+print('price')
+print(price)
+#varialble decimal
+price=3.456
+print(price)
+#variable boolean value
+is_var=True
+#exercise
+name="jhon smith"
+age=20
+is_new=True
+#for to find the length
+course="python for beginners"
+print(len(course))
+#for to upper
+course="python"
+print(course.upper())
+#for  converting lower
+var="HI VARSHINI"
+print(var.lower())
+#for finding the some string
+phy="python"
+print(phy.find('y'))
+#for the replace of the string
+var='i am varshini'
+print(var.replace('varshini','nireeksha'))
